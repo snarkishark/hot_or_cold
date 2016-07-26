f7fa3f48adf5e8b0b32b93a7f8ce131411568cdc
snarkishark <snarkishark@gmail.com>
2016-07-26 18:42:51 -0400

    second commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,62 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	newGame();
+
+  	$(".new").on("click",function(e){
+  		newGame();
+  	});
+  
+  	$("form").on("submit",function(e){
+  		event.preventDefault();
+  		var userGuess = $("#userGuess").val();
+  		if ($.isNumeric(userGuess)){
+  			if(100>= userGuess >= 1){
+  			assessGuess(userGuess);
+  			}else{
+  				alert("Your guess must be a number between 1 and 100!");
+  			}
+  		}else{
+  			alert("Your guess must be a number between 1 and 100!");
+  		}
+  	});
+
 });
 
+var secretNumber;
+var count = 0;
 
+function newGame(){
+	
+	$("#count").html("0");
+	count = 0;
+	$("#userGuess").attr("placeholder","Enter your Guess");
+	$("#guessList").empty();
+	secretNumber = Math.floor(Math.random()*100);
+}
+
+function assessGuess(userGuess){
+	
+	
+	console.log("guess is "+userGuess);
+	var difference=Math.abs(secretNumber - userGuess);
+
+	if (difference>=50){
+		$("h2#feedback").html("Ice cold!");
+	}else if (difference>=20){
+		$("h2#feedback").html("Cold!");
+	}else if(difference>=10){
+		$("h2#feedback").html("Lukewarm");
+	}else if(difference>=3){
+		$("h2#feedback").html("Warm!");
+	}else if(difference>=0){
+		$("h2#feedback").html("Hot!");
+	}else if(difference == 0){
+		$("h2#feedback").html("You got it!");
+	}
+	$("#guessList").append("<li>"+userGuess+"</li>");
+	count+=1;
+	$("#count").html(count);
+
+
+}
